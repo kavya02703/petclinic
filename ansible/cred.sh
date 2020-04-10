b105debe7e0b8191cc815ac1619c729327b1139d
kavya02703 <63443791+kavya02703@users.noreply.github.com>
2020-04-10 14:51:06 +0530

    kavya

--- ansible/cred.sh
+++ ansible/cred.sh
@@ -1,3 +1,3 @@
-export AWS_ACCESS_KEY_ID=**********AKIAVKALHN45R3FZ43FQ************
-export AWS_SECRET_ACCESS_KEY=**************WgFako0goxnWyqmBSnICaXtINrPVpgQ9FcuoJfpi********
+export AWS_ACCESS_KEY_ID=**********Access-key************
+export AWS_SECRET_ACCESS_KEY=**************secret-key********
 export AWS_DEFAULT_REGION=eu-central-1
